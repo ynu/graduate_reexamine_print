f8a971033d69666f69073b89b3310a9a37e82f61
liudonghua <liudonghua123@gmail.com>
2017-03-24 11:57:44 +0800

    update convert script

--- resources/convert-to-pdf-batch.bat
+++ resources/convert-to-pdf-batch.bat
@@ -3,12 +3,10 @@
 echo "begin transform"
 time /t
 
-for /R "D:\code\graduate_reexamine_print\resources\generated_notes\" %%f in (*.docx) do (
+for /R "generated_notes\" %%f in (*.docx) do (
   echo OfficeToPDF.exe %%f generated_notes
   OfficeToPDF.exe %%f generated_notes
 )
 
 echo "end transform"
 time /t
-
-pause
